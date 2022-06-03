620c549d128c7f63f35f79d9352b878c2b9a6db0
bange-git <sebastienbange@gmail.com>
2022-06-03 16:29:08 +0100

    complete area and line charts implementation

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -7,7 +7,9 @@ import {
   DateTime,
   Legend,
   Tooltip,
+  LineSeries,
 } from '@syncfusion/ej2-react-charts';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 import {
   lineCustomSeries,
@@ -16,7 +18,24 @@ import {
 } from '../../data/dummy';
 
 const LineChart = () => {
-  return <ChartComponent></ChartComponent>;
+  const { currentMode } = useStateContext();
+  return (
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export default LineChart;
